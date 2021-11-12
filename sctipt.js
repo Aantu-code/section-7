12f74e02542462787fc02e97759bae79c7c88695
aantu <jibonoshomapto3@gmail.com>
2021-11-12 12:15:28 -0800

    repo commit

---
+++ sctipt.js
@@ -0,0 +1,20 @@
+// (function ($){
+// jQuery(document).ready(function($){
+
+
+// $(".embed-responsive iframe").addClass("embed-responsive-item");
+// $(".carousel-inner .item:first-child").addClass("active");
+// $('[.data-toggle="toolpit"]').toolpit();
+
+
+// })
+
+
+
+
+// }(jQuery))
+$(document).ready(function(){
+    $(".staff-list").owlCarousel({
+        items:5
+    });
+  });
